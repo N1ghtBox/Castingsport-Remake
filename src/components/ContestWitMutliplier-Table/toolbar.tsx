98e37266fb409d3a14f2722020373769bb451c17
DawidWitczak <d.witczak@a-soft.pl>
2025-08-31 21:35:57 +0200

    hotfix

--- src/components/ContestWitMutliplier-Table/toolbar.tsx
+++ src/components/ContestWitMutliplier-Table/toolbar.tsx
@@ -4,13 +4,11 @@ import {
 	type GridSlotProps,
 	GridToolbarContainer,
 } from "@mui/x-data-grid";
+import { EditIcon } from "lucide-react";
 import CategoryCombobox from "../ui/CategoryCombobox";
 import PrintButton from "../ui/PrintButton";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
-import { EditIcon } from "lucide-react";
-import { CompetitonContext } from "@/types/CompetitionContext";
-import { useContext } from "react";
 
 declare module "@mui/x-data-grid" {
 	interface ToolbarPropsOverrides {
@@ -22,7 +20,6 @@ declare module "@mui/x-data-grid" {
 }
 
 export function EditToolbar(props: GridSlotProps["toolbar"]) {
-		const { searchValue, setSearchValue } = useContext(CompetitonContext)
 	const saveAllPendingChanges = () => {
 		for (let i = 0; i < props.pendingRows.length; i++) {
 			const element = props.pendingRows[i];
@@ -44,7 +41,6 @@ export function EditToolbar(props: GridSlotProps["toolbar"]) {
 			)}
 
 			<CategoryCombobox />
-			<Input className="w-60" placeholder="Wyszukaj..." value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
 			<PrintButton />
 
 			<Button
